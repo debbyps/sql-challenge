f8bd057fe8f0c9bbf8b1ba2bb7cf1e5a8f861e7d
Debby Pulido <66884955+debbyps@users.noreply.github.com>
2020-08-24 20:06:25 -0700

    updating columns

--- EmployeeSQL/Analysis/Question_4.sql
+++ EmployeeSQL/Analysis/Question_4.sql
@@ -4,11 +4,10 @@
 	first name, and department name.
 */
 SELECT
-	d.dept_no
-	, d.dept_name
-	, e.emp_no
+	e.emp_no
 	, e.last_name
 	, e.first_name
+	, d.dept_name
 FROM 
 	employees e
 	INNER JOIN dept_emp de
